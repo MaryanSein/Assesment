62117b456fe09ba2b744d513106ff763f4605db1
Maryan Hussein <maryanhussein.h@gmail.com>
2023-12-21 12:33:52 +0100

    Third

--- pipline_assesment.py
+++ pipline_assesment.py
@@ -6,7 +6,7 @@ from snowflake.connector.pandas_tools import write_pandas
 file_input = "spotify_songs.csv"
 df = pd.read_csv(file_input)
 
-# cleaning missing values
+# Cleaning missing values
 df["track_name"].fillna("Unknown_track", inplace=True)
 df["track_artist"].fillna("Unknown_artist", inplace=True)
 df["track_album_name"].fillna("Unknown_album", inplace=True)
@@ -14,12 +14,12 @@ df["track_album_name"].fillna("Unknown_album", inplace=True)
 # Extract only the first 4 characters of the 'track_album_release_date' string
 df['track_album_release_date'] = df['track_album_release_date'].astype(str).str[:4]
 
-# Convert 'duration_ms' to minutes and cast to int64
+# Transforming 'duration_ms' to minutes and cast to int64
 df['duration_min'] = (df['duration_ms'] / (1000 * 60)).astype('int64')
 # Drop the original 'duration_ms' column
 df.drop('duration_ms', axis=1, inplace=True)
 
-# Snowflake account credentials and connection details
+# Load Snowflake account credentials and connection details
 user = "MARYAN"
 password = "Hejsan123"  # Replace with your actual password
 account = "DRXDGZU-CS03614"
